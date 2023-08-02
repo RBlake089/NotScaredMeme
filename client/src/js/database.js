a557211f0fad92571e1fa7f05b5981af9e7cf4e4
rblake089 <rblake089@gmail.com>
2023-08-02 19:52:01 -0400

    i put my update db function in a try catch method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,16 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// Method to put content into the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  console.log('PUT db');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log('data saved', result.value);
-}
-
+  try {
+    console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    await request;
+    console.log('data saved to the database', content);
+  } catch (error) {
+    console.error('Error putting data into the database', error);
+  }
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
@@ -31,10 +35,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  result
-    ? console.log('data retrieved', result.value)
-    : console.log('data not found');
- 
+  
 }
 
 initdb();
