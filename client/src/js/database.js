754923ecfe63f3ff11a61536fc7c7591fc4e02c0
rblake089 <rblake089@gmail.com>
2023-08-02 19:55:57 -0400

    updated my read from db function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -42,15 +42,54 @@ export const putDb = async (content) => {
     console.error('Error putting data into the database', error);
   }
 };
+
+
+
 // TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  
-}
+  try {
+    // Logging a message to indicate that we are attempting to get data from the database
+    console.log('GET from the database');
+    
+    // Opening the 'jate' database with version 1
+    const jateDb = await openDB('jate', 1);
+    
+    // Starting a new transaction on the 'jate' object store with readonly access
+    const tx = jateDb.transaction('jate', 'readonly');
+    
+    // Getting a reference to the 'jate' object store
+    const store = tx.objectStore('jate');
+    
+    // Getting the data from the 'jate' object store using the key '1'
+    const request = store.get(1);
+    
+    // Waiting for the 'get' operation to complete and obtain the result
+    const result = await request;
+    
+    // Checking if the result is not null (data found in the database)
+    if (result) {
+      // Logging a message to indicate that the data has been successfully retrieved from the database
+      console.log('Data retrieved from the database', result.value);
+      
+      // Returning the retrieved data
+      return result.value;
+    } else {
+      // Logging a message to indicate that the data was not found in the database
+      console.log('Data not found in the database');
+      
+      // Returning null to indicate that no data was found
+      return null;
+    }
+  } catch (error) {
+    // Handling any errors that occurred during the database operation
+    console.error('Error getting data from the database', error);
+    
+    // Returning null in case of an error
+    return null;
+  }
+};
+
+
 
 initdb();
